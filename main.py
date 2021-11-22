c11eb0fd7c281d8ee797c78dad3c1356102ef892
FlukerBr <86746681+FlukerBr@users.noreply.github.com>
2021-11-22 00:46:28 -0300

    remove line code

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from PySimpleGUI.PySimpleGUI import theme_text_element_background_color
 import pyttsx3
 import PySimpleGUI as sg
 def falar(texto):
     engine = pyttsx3.init('sapi5')
     engine.setProperty('rate', 180)
-    voices = engine.getProperty('voices')
-    try:
-        engine.setProperty('voice', voices[3].id)
-    except IndexError:
-        engine.setProperty('voice', voices[0].id)
     engine.save_to_file(texto, 'output.mp3')
     engine.runAndWait()
 sg.theme('LightBlue')
